03a5f364b4cdb0a880102df01e62a449c856dc1c
Halil Ömer <halilomer03@gmail.com>
2019-06-09 17:12:51 +0300

    Added FRC Analog Gyro Code

---
+++ src/main/java/frc/robot/sensors/analogGyro.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.sensors;
+
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
+/**
+ * Add your docs here.
+ */
+public class analogGyro {
+public Gyro gyro;
+public double angle;
+    
+    public analogGyro(){
+        gyro = new AnalogGyro(1);
+    }
+    public double angleValue(){
+        angle = gyro.getAngle();
+        return angle;
+    }
+}
